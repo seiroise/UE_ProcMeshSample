b573839fca41bdfd674dadf90601b363928a7c8e
seiroise <seiroise@gmail.com>
2024-02-25 15:56:53 +0900

    add planet sample

---
+++ Source/ProcMeshSample/Common/ScopedTimer.cpp
@@ -0,0 +1,4 @@
+// Copyright seiroise.
+
+
+#include "ScopedTimer.h"
---
+++ Source/ProcMeshSample/Common/ScopedTimer.h
@@ -0,0 +1,16 @@
+// Copyright seiroise.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ScopedTimer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCMESHSAMPLE_API UScopedTimer : public UObject
+{
+	GENERATED_BODY()
+};
